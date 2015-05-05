e36fdd9484aed89f3f4e19294687e3187975419f
Jake Ginnivan <jake@ginnivan.net>
2015-05-05 23:07:17 +0100

    Columns need to float to stack horizontally

--- lib/react-flex-layout.jsx
+++ lib/react-flex-layout.jsx
@@ -66,22 +66,27 @@ export default class Layout extends React.Component {
   render() {
     var width = this.props.layoutWidth === 'flex' ? this.props.calculatedFlexWidth : (this.props.layoutWidth || this.state.layoutWidth)
     var height = this.props.layoutHeight === 'flex' ? this.props.calculatedFlexHeight : (this.props.layoutHeight || this.state.layoutHeight)
-    var style = {
-      width: width,
-      height: height
-    }
+    var style = this.props.style || {}
+    style.width = width
+    style.height = height
     var calculatedFlexWidth = this.state.calculatedFlexWidth
     var calculatedFlexHeight = this.state.calculatedFlexHeight
     var children = React.Children.map(
       this.props.children,
       child => {
-        if (child instanceof React.Component) {
-          var cloned = React.cloneElement(child, {
+        if (child._isReactElement) {
+          var newProps = {
             calculatedFlexWidth: calculatedFlexWidth,
             calculatedFlexHeight: calculatedFlexHeight,
             layoutWidth: child.props.layoutWidth || width,
             layoutHeight: child.props.layoutHeight || height
-          })
+          }
+          if (calculatedFlexWidth){
+            var childStyle = child.props.style || {}
+            childStyle.float = 'left'
+            newProps.style = childStyle
+          }
+          var cloned = React.cloneElement(child, newProps)
           return cloned
         }
         return child
